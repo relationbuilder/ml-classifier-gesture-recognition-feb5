d600a560debf50334b55d2b2d47eae539c9ffb5a
jwork <joee@a2wh.com>
2017-01-15 18:44:20 -0800

    working on go port of classifier

---
+++ makeGO.bat
@@ -0,0 +1,4 @@
+call setGOEnv
+go build src/splitCSVFile.go
+go build src/classifyTest.go
+go build src/csvInfoTest.go
---
+++ splitData.bat
@@ -0,0 +1,6 @@
+:: Run the basic split data to create the 
+:: files we need for the test code 
+
+
+splitCSVFile data/titanic.csv 10
+splitCSVFile data/breast-cancer-wisconsin.adj.data.csv 15
