2dd8a78980517126234fa5cee0c00d2948d1f7b7
jwork <joee@a2wh.com>
2017-02-04 18:24:15 -0800

    Merge  jim's changes into current branch

---
+++ classifyTestStockslv.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#rm classifyFiles.exe
+#go build src/classifyFiles.go
+#
+# Attempts to classify data based on which bars will
+# go up by 1` before they drop by 1`.  With the assumption
+# that greater than 50% accuracy could be profitable 
+# depending on trading fees.   Those that go up are 
+# classified as 1 those that drop are classified as 0
+# The classification step is done by stock-prep-sma.py 
+# which also splits the data between train and test file.
+#
+# To Run the Stock example on SPY data first run the 
+# the utility to download data from yahoo python yahoo-stock-download.py
+# then run utility to convert raw stock data into machine learning
+#  data python stock-prep-sma.py then  you can run
+# this module.
+classifyFiles -train=data/slv.slp30.train.csv -test=data/slv.slp30.test.csv -maxBuck=160 -testOut=tmpout/slv.slp30.out.csv 
---
+++ makeGO.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+
+go build src/splitCSVFile.go
+go build src/classifyTest.go
+go build src/csvInfoTest.go
+go build src/classifyFiles.go
+
